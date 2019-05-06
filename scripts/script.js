9a511ad1c39ad895a8d4bf52898e520e1bb8fdd5
marty4991 <zotic.marian@yahoo.com>
2019-05-06 09:10:22 +0300

    Hold shift checkboxes

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+	// Check if they had the shift key down
+	//And check that they are checking it
+
+	let inBetween =false;
+
+	if(e.shiftKey && this.checked) {
+		// go ahead and do what we please
+		// loop over single checkbox
+		checkboxes.forEach(checkbox => {
+			console.log(checkbox);
+			if(checkbox === this || checkbox === lastChecked) {
+				inBetween = !inBetween
+				console.log('Starting checking in between');
+			}
+
+	if(inBetween) {
+		checkbox.checked = true;
+	}
+		});
+	}
+	
+
+
+	lastChecked= this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+
+var i = 0; 
+for (; i < 9; i++){
+console.log(i);
+}
